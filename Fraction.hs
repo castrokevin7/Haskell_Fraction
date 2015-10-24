498d5f3da899cb32549a4af85d62f693d5a93780
root <root@localhost.localdomain>
2015-10-23 19:32:19 -0600

    Comments added

--- Fraction.hs
+++ Fraction.hs
@@ -1,46 +1,45 @@
 module Fraction where
-
 import Data.Ratio
 
 infix 7 :/
 
-data Fraction = Integer :/ Integer
+data Fraction = Integer :/ Integer  -- Constructor.
     deriving (Eq)
 
-reduce  :: Fraction -> Fraction
-reduce (num:/0) =
+reduce  :: Fraction -> Fraction     -- Fraction reducer.
+reduce (num:/0) =                   -- Denominator equals zero: do anything.
     if num < 0 
     then (-1):/0
     else 1:/0
 
-reduce (num:/den) =
-    (u `quot` d):/(v `quot` d)
+reduce (num:/den) =                 -- Simplification.
+    (quot u d):/(quot v d)
     where
-        d      = gcd u v
+        d      = gcd u v            -- Great common divisor.
         (u, v) =
             if den < 0 
             then (-num, -den)
             else (num, den)
 
-(//) :: Integer -> Integer -> Fraction
+(//) :: Integer -> Integer -> Fraction -- Fraction common reader (includes reduce)
 num // den = reduce (num:/den)
 
-instance Read Fraction where
+instance Read Fraction where            -- // Reader.
     readsPrec p =
         readParen (p > 7) (\r -> [(num//den, u) | (num, s)  <- reads r,
                                                   ("//", t) <- lex s,
                                                   (den, u)  <- reads t])
 
-instance Show Fraction where
+instance Show Fraction where            -- Fraction show.
     showsPrec p (num:/den) =
-        if den == 1
-        then showsPrec p num
+        if den == 1                     -- If denominator equals 1,
+        then showsPrec p num            -- show only the numerator.
         else showParen (p > 7) (shows num . showString "/" . shows den)
 
-instance Ord Fraction where
+instance Ord Fraction where             -- Fraction compare.
     compare (num:/den) (num':/den') = compare (num * den') (num' * den)
 
-instance Num Fraction where
+instance Num Fraction where             -- Fraction numerables: + - *.
     (num:/den) + (num':/den')  = reduce ((num * den' + num' * den):/(den * den'))
     (num:/den) - (num':/den')  = reduce ((num * den' - num' * den):/(den * den'))
     (num:/den) * (num':/den')  = reduce ((num * num'):/(den * den'))
@@ -49,16 +48,16 @@ instance Num Fraction where
     signum (num:/_)   = signum num:/1
     fromInteger n     = fromInteger n:/1
 
-instance Fractional Fraction where
-    (num:/0) / (num':/0) = ((signum num * signum num'):/0)
+instance Fractional Fraction where      -- Fraction fractional: /.
+    (num:/0) / (num':/0) = (signum num * signum num'):/0
     (_:/_) / (_:/0)      = (0:/1)
     (num:/0) / (_:/_)    = (num:/0)
-    (num:/den) / (num':/den') = reduce ((num * den') :/ (den * num'))
+    (num:/den) / (num':/den') = reduce ((num * den'):/(den * num'))
     recip (num:/den)          = 
         if num < 0 
         then (-den):/(-num) 
         else den:/num    
-    fromRational a = num:/den
+    fromRational a = num:/den 
         where
             num = numerator a
             den = denominator a
