d3614521090898c7e404954a6d2638c54dbea16b
root <root@localhost.localdomain>
2015-11-04 08:44:06 -0600

    Comments added

--- Fraction.hs
+++ Fraction.hs
@@ -6,25 +6,25 @@ infix 7 :/
 data Fraction = Integer :/ Integer  -- Constructor.
     deriving (Eq)
 
-reduce  :: Fraction -> Fraction     -- Fraction reducer.
-reduce (num:/0) =                   -- Denominator equals zero: do anything.
+simplify  :: Fraction -> Fraction   -- Fraction simplifier.
+simplify (num:/0) =                 -- Denominator equals zero: do anything.
     if num < 0 
     then (-1):/0
     else 1:/0
 
-reduce (num:/den) =                 -- Simplification.
-    (quot u d):/(quot v d)
+simplify (num:/den) =               -- Simplification.
+    (quot x d):/(quot x' d)
     where
-        d      = gcd u v            -- Great common divisor.
-        (u, v) =
+        d       = gcd x x'          -- Great common divisor.
+        (x, x') =
             if den < 0 
             then (-num, -den)
             else (num, den)
 
-(//) :: Integer -> Integer -> Fraction -- Fraction common reader (includes reduce)
-num // den = reduce (num:/den)
+(//) :: Integer -> Integer -> Fraction 	-- Fraction common reader (includes simplify)
+num // den = simplify (num:/den)
 
-instance Read Fraction where            -- // Reader.
+instance Read Fraction where       		-- // Reader.
     readsPrec p =
         readParen (p > 7) (\r -> [(num//den, u) | (num, s)  <- reads r,
                                                   ("//", t) <- lex s,
@@ -40,9 +40,9 @@ instance Ord Fraction where             -- Fraction compare.
     compare (num:/den) (num':/den') = compare (num * den') (num' * den)
 
 instance Num Fraction where             -- Fraction numerables: + - *.
-    (num:/den) + (num':/den')  = reduce ((num * den' + num' * den):/(den * den'))
-    (num:/den) - (num':/den')  = reduce ((num * den' - num' * den):/(den * den'))
-    (num:/den) * (num':/den')  = reduce ((num * num'):/(den * den'))
+    (num:/den) + (num':/den')  = simplify ((num * den' + num' * den):/(den * den'))
+    (num:/den) - (num':/den')  = simplify ((num * den' - num' * den):/(den * den'))
+    (num:/den) * (num':/den')  = simplify ((num * num'):/(den * den'))
     negate (num:/den) = negate num:/den
     abs (num:/den)    = abs num:/den
     signum (num:/_)   = signum num:/1
@@ -52,7 +52,7 @@ instance Fractional Fraction where      -- Fraction fractional: /.
     (num:/0) / (num':/0) = (signum num * signum num'):/0
     (_:/_) / (_:/0)      = (0:/1)
     (num:/0) / (_:/_)    = (num:/0)
-    (num:/den) / (num':/den') = reduce ((num * den'):/(den * num'))
+    (num:/den) / (num':/den') = simplify ((num * den'):/(den * num'))
     recip (num:/den)          = 
         if num < 0 
         then (-den):/(-num) 
